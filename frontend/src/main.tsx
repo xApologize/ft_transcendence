ee242e6a6eb43bbc21bc2f013fffa4f173692f45
xApologize <jenbenrossignol@hotmail.com>
2023-10-10 16:24:57 -0400

    add react router in the main.tsx to route back to home pages instead of app.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
 import './index.css'
+import Home from "./Pages/Home";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home/>}/>
+                <Route path={"/Home"} element={<Home/>}/>
+            </Routes>
+        </BrowserRouter>
+      </>
   </React.StrictMode>
 )
