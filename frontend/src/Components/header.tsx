28048abd44057bb58b1ecc948f85e1708394c6ff
xApologize <jenbenrossignol@hotmail.com>
2023-10-16 14:35:45 -0400

    mess around with navbar

--- frontend/src/Components/header.tsx
+++ frontend/src/Components/header.tsx
@@ -1,14 +1,20 @@
-import {AppBar, Container} from "@mui/material";
+import { AppBar, Container, Typography, Toolbar } from '@mui/material'
+import Adbicon from '@mui/icons-material/adb'
 function header() {
     return (
         <>
-            <AppBar position="static">
-                <Container max>
-
+            <AppBar position="static" color="secondary">
+                <Container fixed>
+                    <Toolbar disableGutters variant="dense">
+                        <Adbicon
+                            sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}
+                        />
+                        <Typography>Transcendance</Typography>
+                    </Toolbar>
                 </Container>
             </AppBar>
         </>
     )
 }
 
-export default header;
+export default header
