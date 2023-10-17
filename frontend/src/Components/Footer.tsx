742db759963e6575ee715768e92e63197803a038
xApologize <jenbenrossignol@hotmail.com>
2023-10-17 18:39:21 -0400

    add style to the footer

--- frontend/src/Components/Footer.tsx
+++ frontend/src/Components/Footer.tsx
@@ -1,16 +1,39 @@
-import { AppBar, Toolbar } from '@mui/material';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 import styled from '@emotion/styled';
 
 const StyledAppBarFooter = styled(AppBar)`
-    height: 30px;
+    height: 32px;
     top: auto;
     bottom: 0;
+`;
+
+const StyledToolbar = styled(Toolbar)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const StyledCopyrightDiv = styled.div`
+    height: 55px;
+`;
+
+const StyledAboutUsDiv = styled.div`
+    position: absolute;
+    right: 20px;
+    height: 55px;
 `;
 
 function Footer() {
     return (
         <StyledAppBarFooter color="secondary">
-            <Toolbar></Toolbar>
+            <StyledToolbar>
+                <StyledCopyrightDiv>
+                    <Typography>Copyright &copy;</Typography>
+                </StyledCopyrightDiv>
+                <StyledAboutUsDiv>
+                    <Typography>about us</Typography>
+                </StyledAboutUsDiv>
+            </StyledToolbar>
         </StyledAppBarFooter>
     );
 }
