ab9553924afe7238b92287c7d7dc4be521968306
xApologize <jenbenrossignol@hotmail.com>
2023-10-17 16:03:41 -0400

    add new component for the game and chat

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,10 +1,32 @@
 import Header from "../Components/header";
+import Chat from '../Components/chat';
+import Game from '../Components/game';
+import styled from '@emotion/styled';
+
+const MainLayout = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-width: 100%;
+    height: 100vh;
+`;
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    min-width: 100%;
+    height: 100%;
+`;
 function Home() {
     return (
-        <>
-            <Header/>
-        </>
-    )
+        <MainLayout>
+            <Header />
+            <Container>
+                <Chat />
+                <Game />
+            </Container>
+        </MainLayout>
+    );
 }
 
 export default Home;
