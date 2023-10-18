ef4250fee8a33cac54dbeee97a95e6a2c3c45381
xApologize <jenbenrossignol@hotmail.com>
2023-10-18 10:34:48 -0400

    prettier import change

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-} from "react-router-dom";
-import './index.css'
-import Home from "./Pages/Home";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Home from './Pages/Home';
 import Home1 from './Pages/Home1';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
