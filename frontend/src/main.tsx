e58a111708b7414f63f114b5bd5289d710938919
xApologize <jenbenrossignol@hotmail.com>
2023-10-18 07:40:59 -0400

    add temporary page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,16 +7,18 @@ import {
 } from "react-router-dom";
 import './index.css'
 import Home from "./Pages/Home";
+import Home1 from './Pages/Home1';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-      <>
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Home/>}/>
-                <Route path={"/Home"} element={<Home/>}/>
-            </Routes>
-        </BrowserRouter>
-      </>
-  </React.StrictMode>
-)
+    <React.StrictMode>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path={'/Home'} element={<Home />} />
+                    <Route path={'/Home1'} element={<Home1 />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    </React.StrictMode>
+);
