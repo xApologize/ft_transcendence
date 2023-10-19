adbb4b2201711d21eb159b60cbba05b9dec086c4
xApologize <jenbenrossignol@hotmail.com>
2023-10-19 14:01:58 -0400

    add profile to route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './Pages/Home';
+import Profile from './Pages/Profile';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
                 <Routes>
                     <Route index element={<Home />} />
                     <Route path={'/Home'} element={<Home />} />
+                    <Route path={'/Profile'} element={<Profile/>} />
                 </Routes>
             </BrowserRouter>
         </>
