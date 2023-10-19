991558858cddafd2e1aa7bc8de39939605717ccd
xApologize <jenbenrossignol@hotmail.com>
2023-10-19 13:03:00 -0400

    remove sencond page layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './Pages/Home';
-import Home1 from './Pages/Home1';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
@@ -12,7 +11,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
                 <Routes>
                     <Route index element={<Home />} />
                     <Route path={'/Home'} element={<Home />} />
-                    <Route path={'/Home1'} element={<Home1 />} />
                 </Routes>
             </BrowserRouter>
         </>
