766c7866f24f014d496d92d1c70482fe24bbae08
xApologize <jenbenrossignol@hotmail.com>
2023-10-19 15:29:28 -0400

    add new route and remove fragment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,17 +4,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './Pages/Home';
 import Profile from './Pages/Profile';
+import NotFound from './Pages/404page';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route index element={<Home />} />
-                    <Route path={'/Home'} element={<Home />} />
-                    <Route path={'/Profile'} element={<Profile/>} />
-                </Routes>
-            </BrowserRouter>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path={'/Home'} element={<Home />} />
+                <Route path={'/Profile'} element={<Profile />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 );
