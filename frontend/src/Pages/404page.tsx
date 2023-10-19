fbf7c83a31d80ff451299d3b7bcaddebe314bd76
xApologize <jenbenrossignol@hotmail.com>
2023-10-19 16:16:03 -0400

    add new 404 page

--- frontend/src/Pages/404page.tsx
+++ frontend/src/Pages/404page.tsx
@@ -1,21 +1,28 @@
 import styled from '@emotion/styled';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import { useNavigate } from 'react-router-dom';
 
 const StyledPageNotFound = styled.div`
     height: 100%;
     width: 100%;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
 `;
 
 function NotFound() {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/Home');
+    };
     return (
         <>
             <Header />
             <StyledPageNotFound>
-                <p>Where the hell are you going?</p>
+                <h2>Where the hell are you going?</h2>
+                <h4 onClick={handleClick}>Go back to safety</h4>
             </StyledPageNotFound>
             <Footer />
         </>
