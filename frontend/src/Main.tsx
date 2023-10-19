2496223088844dc28e5142a2fe58a936349e8489
xApologize <jenbenrossignol@hotmail.com>
2023-10-19 16:57:08 -0400

    Add route and change file name

--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './Pages/Home';
 import Profile from './Pages/Profile';
+import Settings from './Pages/Settings';
+import AboutUs from './Pages/AboutUs';
 import NotFound from './Pages/404page';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -13,6 +15,8 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
                 <Route index element={<Home />} />
                 <Route path={'/Home'} element={<Home />} />
                 <Route path={'/Profile'} element={<Profile />} />
+                <Route path={'/Settings'} element={<Settings />} />
+                <Route path={'/AboutUs'} element={<AboutUs />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
