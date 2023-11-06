c2b73b4fd471085dc8889a347d80931302840d59
producks <davetrap.demers@gmail.com>
2023-11-06 15:29:58 +0000

    Added more GET logic and unitest

--- backend/user_profile/tests.py
+++ backend/user_profile/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import User
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(
+            nickname="Test",
+            email="test@gmail.com",
+            avatar="geropgjieriogjer",
+            status="O",
+            admin=False
+        )
+
+    def test_user(self):
+        user = User.objects.get(nickname="Test")
+        self.assertEqual(user.nickname, "Test")
+        self.assertEqual(user.email, "test@gmail.com")
--- backend/user_profile/views.py
+++ backend/user_profile/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, JsonResponse
 from .models import User
+from django.core import serializers
 
 
 def index(request: HttpRequest):
     return HttpResponse("This is a test!")
 
 def bozo(request: HttpRequest):
-    print(request.GET)
+    print(request.get_full_path)
     if request.method == "GET":
-        return HttpResponse("Hello! Get!")
+        try:
+            user = User.objects.get(pk=1)
+            user_data = {
+                'id': user.id,
+                'nickname': user.nickname,
+                'email': user.email,
+                'avatar': user.avatar,
+                'status': user.status,
+                'admin': user.admin,
+            }
+            return JsonResponse(user_data)
+        except:
+            return HttpResponse("Couldn't find the user")
     elif request.method == "POST":
          return HttpResponse("Hello! POST!")
