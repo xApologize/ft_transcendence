0d72026d3b6f5a65861e1272a0571ab0ba4e80f0
Your Name <you@example.com>
2023-11-07 18:24:54 +0000

    rm useless urls

--- backend/user_profile/urls.py
+++ backend/user_profile/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from user_profile.views import Users
 
 urlpatterns = [
-    # path('<str:user_nickname>/', SpecificUser.as_view(), name="specific_user"),
     path('', Users.as_view(), name="users"),
 ]
