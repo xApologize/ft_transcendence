3390af34a6e72ac2dee6043c4343983156df0a48
Your Name <you@example.com>
2023-11-08 15:48:17 +0000

    add test + doc to test

--- backend/friend_list/tests.py
+++ backend/friend_list/tests.py
@@ -35,24 +35,39 @@ class UserTestCase(TestCase):
 
 
     def test_get_friend(self):
+        """
+        Test retrieving all friend with the default.
+        """
         response = self.client.get(f"{reverse('friendlist')}?nickname=BozoCat")
         self.assertEqual(response.status_code, 200)
+
         friends = response.json()['friends'][0]
         self.assertEqual(friends['friend1'], "BozoCat")
         self.assertEqual(friends['friend2'], "BozoDog")
         self.assertEqual(friends['status'], "ACCEPTED")
 
-
     def test_get_friend_with_status(self):
+        """
+        Test retrieving a friend with a specific status (ACCEPTED).
+        """
         response = self.client.get(f"{reverse('friendlist')}?nickname=BozoCat&status=ACCEPTED")
         self.assertEqual(response.status_code, 200)
 
+        friends = response.json()['friends'][0]
+        self.assertEqual(friends['friend1'], "BozoCat")
+        self.assertEqual(friends['friend2'], "BozoDog")
+        self.assertEqual(friends['status'], "ACCEPTED")
 
     def test_not_recognized_status(self):
+        """
+        Test retrieving a friend with an unrecognized status (BOZO).
+        """
         response = self.client.get(f"{reverse('friendlist')}?nickname=BozoCat&status=BOZO")
         self.assertEqual(response.status_code, 400)
 
-
     def test_get_no_nickname(self):
+        """
+        Test retrieving a friend without specifying a nickname (bad request).
+        """
         response = self.client.get(f"{reverse('friendlist')}")
         self.assertEqual(response.status_code, 400)
