7c1453d2b8890445ac9b393ec62d480091fe68a6
Your Name <you@example.com>
2023-11-14 17:49:13 +0000

    change minor thing

--- frontend/js/components/header/header.js
+++ frontend/js/components/header/header.js
@@ -3,7 +3,6 @@ import { loadHTMLComponent } from '../../api/fetchData.js';
 export async function headerComponent() {
   try {
     const header = await loadHTMLComponent('./js/components/header/header.html');
-    console.log(header);
     return header
   } catch (error) {
     console.error('Error fetching headerComponent:', error);
