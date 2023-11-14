44392e4877caf117c2cdb3318ab70c86053dc91a
Your Name <you@example.com>
2023-11-14 19:02:42 +0000

    add a 'template' for launching game

---
+++ frontend/js/pages/home/gameModal.js
@@ -0,0 +1,31 @@
+class GameModal {
+    constructor(modalId) {
+      this.modal = new bootstrap.Modal(document.getElementById(modalId));
+      this.fullHeader = document.getElementById('full-header');
+    }
+  
+    show() {
+      this.modal.show();
+    }
+  
+    hide() {
+      this.modal.hide();
+    }
+  
+    setBackdrop(backdrop) {
+      this.modal._config.backdrop = backdrop;
+    }
+  
+    removeCloseButton() {
+      const closeBtn = this.modal._element.querySelector('.close');
+      if (closeBtn) {
+        document.getElementById('full-header').removeChild(closeBtn);
+      }
+    }
+
+    hideTitle() {
+      this.fullHeader.style.display = 'none';
+    }
+  }
+
+  export default GameModal;
