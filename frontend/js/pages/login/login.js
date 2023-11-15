8734d360e67b7036605742a7125577a28ded2bd4
xApologize <jenbenrossignol@hotmail.com>
2023-11-15 13:26:59 -0500

    add css to the page

--- frontend/js/pages/login/login.js
+++ frontend/js/pages/login/login.js
@@ -1,9 +1,23 @@
 import { loadHTMLPage } from '../../api/fetchData.js';
 
 export async function showLogin() {
-  try {
-    await loadHTMLPage('./js/pages/login/login.html')
-  } catch (error) {
-    console.error('Error fetching home.html:', error);
-  }
+    try {
+        await loadHTMLPage('./js/pages/login/login.html');
+
+        document.getElementById('loginButton').addEventListener('click', () => {
+            login();
+        });
+    } catch (error) {
+        console.error('Error fetching home.html:', error);
+    }
+}
+
+async function login() {
+    const email = document.getElementById('inputEmail').value;
+    const password = document.getElementById('inputPassword').value;
+    if (!email || !password) {
+        alert('Fill the form.');
+        return;
+    }
+    console.log('login with: ', email, password);
 }
