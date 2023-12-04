60d82d5b18663fd114c3e4f6436335b7ec90eaee
producks <davetrap.demers@gmail.com>
2023-12-04 01:58:47 -0500

    Fixed host bug that was added previously

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -30,11 +30,7 @@ DEBUG = bool(os.environ.get("DEBUG", default=0))
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a
 #  space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = [
-    socket.gethostname(),
-    "localhost",
-    "127.0.0.1"
-]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 INSTALLED_APPS = [
