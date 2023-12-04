88f32dcbc3216f8852a611215a0cd175ef3db8c6
xApologize <jenbenrossignol@hotmail.com>
2023-12-04 16:21:56 -0500

    prettier

--- frontend/js/router.js
+++ frontend/js/router.js
@@ -1,4 +1,4 @@
- import { showHome } from './pages/home/home.js';
+import { showHome } from './pages/home/home.js';
 import { showUser } from './pages/user/user.js';
 import { showGame } from './pages/game/game.js';
 import { showAbout } from './pages/about/about.js';
@@ -13,59 +13,58 @@ import { showSocket } from './pages/socket/socket.js';
 
 var currentRoute = '';
 const routes = {
-  '/': showLogin,
-  '/home': showHome,
-  '/about': showAbout,
-  '/game_page': showGame,
-  '/user': showUser,
-  '/login': showLogin,
-  '/signUp': showSignUp,
-  '/socket': showSocket
+    '/': showLogin,
+    '/home': showHome,
+    '/about': showAbout,
+    '/game_page': showGame,
+    '/user': showUser,
+    '/login': showLogin,
+    '/signUp': showSignUp,
+    '/socket': showSocket,
 };
 
 function showPage(pageFunction) {
-  pageFunction();
+    pageFunction();
 }
 
 export function navigateTo(route) {
-  // console.log("navigateTo!")
-  if (route === currentRoute)
-    return ;
-  history.pushState(null, null, route);
-  handleRoute();
+    // console.log("navigateTo!")
+    if (route === currentRoute) return;
+    history.pushState(null, null, route);
+    handleRoute();
 }
 
 async function checkIfCookie() {
-  var accessTokenLive = sessionStorage.getItem('jwt');
-  var options = {
-    method: 'GET',
-    credentials: 'include',
-    headers: {
-      ...(accessTokenLive ? { 'jwt': `${accessTokenLive}` } : {}),
-    },
-  };
-  const tokenResponse = await fetch('/api/auth/token/', options)
-  return tokenResponse
+    var accessTokenLive = sessionStorage.getItem('jwt');
+    var options = {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+            ...(accessTokenLive ? { jwt: `${accessTokenLive}` } : {}),
+        },
+    };
+    const tokenResponse = await fetch('/api/auth/token/', options);
+    return tokenResponse;
 }
 
-export async function handleRoute() {  
-  var pageFunction = null;
-  var goPath = window.location.pathname
-  if (goPath == '/home') {
-    var cookieResponse = await checkIfCookie()
-    if (cookieResponse.status == 401) {
-      history.pushState(null, null, '/');
-      goPath = '/'
+export async function handleRoute() {
+    var pageFunction = null;
+    var goPath = window.location.pathname;
+    if (goPath == '/home') {
+        var cookieResponse = await checkIfCookie();
+        if (cookieResponse.status == 401) {
+            history.pushState(null, null, '/');
+            goPath = '/';
+        }
     }
-  }
 
-  if (routes[goPath]) {
-    pageFunction = routes[goPath]
-  } else {
-    pageFunction = show404
-  }
-  currentRoute = goPath
-  showPage(pageFunction);
+    if (routes[goPath]) {
+        pageFunction = routes[goPath];
+    } else {
+        pageFunction = show404;
+    }
+    currentRoute = goPath;
+    showPage(pageFunction);
 }
 
 // !! Do not change the order in which it's append !!
@@ -75,45 +74,45 @@ async function loadPage() {
     const template = await templateComponent();
     // const footer = await footerComponent()
 
-  body.append(header)
-  body.append(template)
-  const path = window.location.pathname;
-  navigateTo(path)
+    body.append(header);
+    body.append(template);
+    const path = window.location.pathname;
+    navigateTo(path);
 }
 
 // Rediriger vers /home if refresh token.
 document.addEventListener('DOMContentLoaded', async () => {
-  // isPathGame()
-  await loadPage()
+    // isPathGame()
+    await loadPage();
 
-  const navContainer = document.getElementById('navbar');
-  navContainer.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.classList.contains('nav-link')) {
-      event.preventDefault();
-      const route = target.getAttribute('data-route');
-      navigateTo(route);
-    }
-  });
+    const navContainer = document.getElementById('navbar');
+    navContainer.addEventListener('click', (event) => {
+        const target = event.target;
+        if (target.classList.contains('nav-link')) {
+            event.preventDefault();
+            const route = target.getAttribute('data-route');
+            navigateTo(route);
+        }
+    });
 
-  window.addEventListener('popstate', handlePopState);
+    window.addEventListener('popstate', handlePopState);
 });
 
 function handlePopState(event) {
-  closeModal()
-  handleRoute(window.location.pathname)
+    closeModal();
+    handleRoute(window.location.pathname);
 }
 
 function closeModal() {
-  const modal = document.getElementById("playModal");
-  if (modal) {
-    if (modal.classList.contains("show")) {
-      modal.classList.remove("show");
-      document.body.classList.remove("modal-open");
-      const backdrop = document.querySelector(".modal-backdrop");
-      if (backdrop) {
-        backdrop.remove();
-      }
+    const modal = document.getElementById('playModal');
+    if (modal) {
+        if (modal.classList.contains('show')) {
+            modal.classList.remove('show');
+            document.body.classList.remove('modal-open');
+            const backdrop = document.querySelector('.modal-backdrop');
+            if (backdrop) {
+                backdrop.remove();
+            }
+        }
     }
-  }
 }
