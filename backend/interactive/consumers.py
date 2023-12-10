ab36470e11e4bb3d9dc12ddf486158a5f09d4e72
producks <davetrap.demers@gmail.com>
2023-12-10 16:03:27 -0500

    leak backup

--- backend/interactive/consumers.py
+++ backend/interactive/consumers.py
@@ -1,6 +1,7 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from interactive.models import LookingForMatch
 from asgiref.sync import sync_to_async
+import json
 
 
 class UserInteractiveSocket(AsyncWebsocketConsumer):
@@ -36,15 +37,15 @@ class UserInteractiveSocket(AsyncWebsocketConsumer):
                 )
 
     async def send_message_echo(self, data: any):
-        await self.send(text_data=data["message"])
+        await self.send(text_data=json.dumps(data["message"]))
 
     async def send_message_no_echo(self, data: any):
         if self.channel_name != data["sender"]:
-            await self.send(text_data=data['message'])
+            await self.send(text_data=json.dumps(data['message']))
     
-    async def send_specific_message(self, data: any):
+    async def send_mailbox_message(self, data: any):
         if self.channel_name == data["sender"]:
-            await self.send(text_data=data["message"])
+            await self.send(text_data=json.dumps(data["message"]))
 
     async def find_match(self):
         match_entry = await sync_to_async(LookingForMatch.objects.filter(paddleB=-1).first)()
@@ -56,17 +57,32 @@ class UserInteractiveSocket(AsyncWebsocketConsumer):
     async def create_lfm(self):
         print("CREATE MATCH")
         await sync_to_async(LookingForMatch.objects.create)(
-                paddleA=self.user_id, paddleB=-1)
+                paddleA=self.user_id, mailbox_a=self.channel_name, paddleB=-1)
         self.waiting: bool = True
 
     async def setup_match(self, match: any):
         print("SETUP MATCH")
         match.paddleB = self.user_id
         await sync_to_async(match.save)()
+        handle_a: dict = await self.create_math_handle(match.paddleA, match.paddleB, "A")
+        handle_b: dict = await self.create_math_handle(match.paddleA, match.paddleB, "B")
         await self.channel_layer.group_send(
-                "interactive", 
-                create_layer_dict("send_message_echo", "Hey! Match is ready", self.channel_name)
+                "interactive",
+                create_layer_dict(
+                    "send_mailbox_message", handle_a, match.mailbox_a)
                 )
+        await self.channel_layer.group_send(
+                "interactive",
+                create_layer_dict("send_mailbox_message", handle_b, self.channel_name)
+                )
+
+    async def create_math_handle(self, first_id: int, second_id: int, paddle: str) -> dict:
+        handle = {
+            "type": "Found Match",
+            "handle": f"ws/pong/{first_id}{second_id}{paddle}",
+            "paddle": paddle
+        }
+        return handle
 
 
 def create_layer_dict(type: str, message: str, sender: str) -> dict:
--- backend/interactive/models.py
+++ backend/interactive/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class LookingForMatch(models.Model):
     paddleA = models.IntegerField()
+    mailbox_a = models.TextField()
     paddleB = models.IntegerField(default=-1)
