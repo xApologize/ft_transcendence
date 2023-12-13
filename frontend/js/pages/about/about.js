7336b61dcac34f3f4ac23550205119e8c6990851
xApologize <jenbenrossignol@hotmail.com>
2023-12-13 16:19:42 -0500

    test of overflow with bootstrap

--- frontend/js/pages/about/about.js
+++ frontend/js/pages/about/about.js
@@ -1,4 +1,6 @@
-export function showAbout() {
-    const contentContainer = document.getElementById('contentContainer');
-    contentContainer.innerHTML = '<h2>Learn more about us on the About Page.</h2>';
-  }
+import { loadHTMLPage } from '../../api/fetchData.js';
+export async function showAbout() {
+    await loadHTMLPage('./js/pages/about/about.html');
+    // const contentContainer = document.getElementById('contentContainer');
+    // contentContainer.innerHTML = '<h2>Learn more about us on the About Page.</h2>';
+}
