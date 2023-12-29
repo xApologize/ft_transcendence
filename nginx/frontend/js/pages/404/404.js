13f63e913877391b49575468a608f15715afb8ff
producks <davetrap.demers@gmail.com>
2023-12-29 11:35:59 -0500

    nuked frontend from nginx

--- nginx/frontend/js/pages/404/404.js
+++
@@ -1,4 +0,0 @@
-export function show404() {
-    const contentContainer = document.getElementById('contentContainer');
-    contentContainer.innerHTML = '<div style="display: flex; align-items: center; justify-content: center; height: 100vh; font-size: 2em;">404 Page not found</div>';
-}
--- nginx/frontend/js/pages/game/worker.js
+++
@@ -1,6 +0,0 @@
-let lastFixedUpdate = new Date().getTime();
-
-// worker.js
-setInterval(function() {
-    postMessage('');
-}, 1000 / 50);
